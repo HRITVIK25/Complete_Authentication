8d07e51ee43e08519a0e312e2415f5753c255ca2
Hritvik Tayal <hritviktayal74@gmail.com>
2025-03-14 00:17:00 +0530

    set up for deployment

--- Frontend/src/store/authStore.js
+++ Frontend/src/store/authStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API_URL =
   import.meta.env.MODE === "development"
-    ? `${import.meta.env.VITE_API_URL}`
+    ? "http://localhost:5173"
     : "/api/auth";
 
 axios.defaults.withCredentials = true; // cookies sent with every response header
