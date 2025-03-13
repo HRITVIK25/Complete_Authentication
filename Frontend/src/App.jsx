96ff7f2f68d841b5a3028ef4a11247e3290d7ef2
Hritvik Tayal <hritviktayal74@gmail.com>
2025-03-13 23:28:16 +0530

    implemeted forgot-password and reset-password functionlity

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,29 +7,30 @@ import LoadingSpinner from "./components/LoadingSpinner";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import EmailVerificationPage from "./pages/EmailVerificationPage";
-import DashboardPage from "./pages/DashboardPage"
+import DashboardPage from "./pages/DashboardPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authStore.js";
 
-
 // protect routes tyhat reuire authentication
-const ProtectedRoute = ({children}) => {
-  const {isAuthenticated, user } = useAuthStore();
-  if(!isAuthenticated){
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+  if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
-  };
+  }
 
-  if(!user.isVerified){
+  if (!user.isVerified) {
     return <Navigate to="/verify-email" replace />;
-  };
+  }
 
   return children;
-}
+};
 
 // redirect authenticated user to home page
 const RedirectAuthenticated = ({ children }) => {
-  const {isAuthenticated, user } = useAuthStore();
+  const { isAuthenticated, user } = useAuthStore();
   if (isAuthenticated && user.isVerified) {
     return <Navigate to="/" replace />;
   }
@@ -38,13 +39,13 @@ const RedirectAuthenticated = ({ children }) => {
 };
 
 function App() {
-  const { isCheckingAuth, checkAuth, isAuthenticated, user } = useAuthStore();
+  const { isCheckingAuth, checkAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  if(isCheckingAuth) return <LoadingSpinner />
+  if (isCheckingAuth) return <LoadingSpinner />;
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
@@ -71,12 +72,14 @@ function App() {
       />
 
       <Routes>
-
-        <Route path="/" element={
-          <ProtectedRoute>
-            <DashboardPage />
-          </ProtectedRoute>
-        } />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
         <Route
           path="/signup"
           element={
@@ -96,6 +99,22 @@ function App() {
         />
 
         <Route path="/verify-email" element={<EmailVerificationPage />} />
+        <Route
+          path="/forgot-password"
+          element={
+            <RedirectAuthenticated>
+              <ForgotPasswordPage />
+            </RedirectAuthenticated>
+          }
+        />
+        <Route
+          path="/reset-password/:token"
+          element={
+            <RedirectAuthenticated>
+              <ResetPasswordPage />
+            </RedirectAuthenticated>
+          }
+        />
       </Routes>
       <Toaster />
     </div>
--- Frontend/src/store/authStore.js
+++ Frontend/src/store/authStore.js
@@ -1,70 +1,145 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_API_URL}` : "/api/auth";
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? `${import.meta.env.VITE_API_URL}`
+    : "/api/auth";
 
 axios.defaults.withCredentials = true; // cookies sent with every response header
 
-export const useAuthStore = create((set)=>({
+export const useAuthStore = create((set) => ({
     user: null,
-    isAuthenticated:false,
-    error:null,
-    isLoading:false,
-    isCheckingAuth:false,
+    isAuthenticated: false,
+    error: null,
+    isLoading: false,
+    isCheckingAuth: false,
+    message: null,
 
-    signup: async (email,password,name) => {
-        set({isLoading:true,error:null});
+    signup: async (email, password, name) => {
+        set({ isLoading: true, error: null });
         try {
-            const response  = await axios.post(`${API_URL}/signup`,{email,password,name});
-            set({user:response.data.user, isAuthenticated:true, isLoading:false})
+        const response = await axios.post(`${API_URL}/signup`, {
+            email,
+            password,
+            name,
+        });
+        set({
+            user: response.data.user,
+            isAuthenticated: true,
+            isLoading: false,
+        });
         } catch (error) {
-            set({error:error.response.data.message || "Error signing up", isLoading:false})
-            throw error;
-        };
+        set({
+            error: error.response.data.message || "Error signing up",
+            isLoading: false,
+        });
+        throw error;
+        }
     },
 
-    login: async(email,password) => {
-        set({isLoading:true,error:null});
+    login: async (email, password) => {
+        set({ isLoading: true, error: null });
         try {
-            const response = await axios.post(`${API_URL}/login`,{email,password});
-            set({user:response.data.user, isAuthenticated:true, isLoading:false,error:null})
+        const response = await axios.post(`${API_URL}/login`, {
+            email,
+            password,
+        });
+        set({
+            user: response.data.user,
+            isAuthenticated: true,
+            isLoading: false,
+            error: null,
+        });
         } catch (error) {
-            set({error:error.response.data.message || "Error logging in", isLoading:false})
-            throw error;
+        set({
+            error: error.response.data.message || "Error logging in",
+            isLoading: false,
+        });
+        throw error;
         }
     },
 
     logout: async () => {
-        set({isLoading:true,error:null});
+        set({ isLoading: true, error: null });
         try {
-            await axios.post(`${API_URL}/logout`);
-            set({user:null, isAuthenticated:false,isLoading:false,error:null})
+        await axios.post(`${API_URL}/logout`);
+        set({
+            user: null,
+            isAuthenticated: false,
+            isLoading: false,
+            error: null,
+        });
         } catch (error) {
-            set({error:"Error logging out", isLoading:false})
-            throw error;
+        set({ error: "Error logging out", isLoading: false });
+        throw error;
         }
     },
 
     verifyEmail: async (code) => {
-        set({isLoading:true,error:null});
+        set({ isLoading: true, error: null });
         try {
-            const response  = await axios.post(`${API_URL}/verify-email`,{code})
-            set({user:response.data.user, isLoading:false, isAuthenticated:true})
+        const response = await axios.post(`${API_URL}/verify-email`, { code });
+        set({
+            user: response.data.user,
+            isLoading: false,
+            isAuthenticated: true,
+        });
         } catch (error) {
-            set({error:error.response?.data?.message || "Error verifying-email", isLoading:false})
-            throw error;
-        };
+        set({
+            error: error.response?.data?.message || "Error verifying-email",
+            isLoading: false,
+        });
+        throw error;
+        }
     },
 
     checkAuth: async () => {
-        set({isCheckingAuth:true,error:null});
+        set({ isCheckingAuth: true, error: null });
         try {
-            const response = await axios.get(`${API_URL}/check-auth`);
-            set({user:response.data.user, isAuthenticated:true, isCheckingAuth:false})
+        const response = await axios.get(`${API_URL}/check-auth`);
+        set({
+            user: response.data.user,
+            isAuthenticated: true,
+            isCheckingAuth: false,
+        });
         } catch (error) {
-            set({error:null, isCheckingAuth:false, isAuthenticated:false})
-            throw error;
-        };
+        set({ error: null, isCheckingAuth: false, isAuthenticated: false });
+        throw error;
+        }
     },
 
+    forgotPassword: async (email) => {
+        set({ isLoading: true, error: null });
+        try {
+        const response = await axios.post(`${API_URL}/forgot-password`, {
+            email,
+        });
+        set({ isLoading: false });
+        } catch (error) {
+        set({
+            error:
+            error.response?.data?.message ||
+            "Error sending forget-password email",
+            isLoading: false,
+        });
+        throw error;
+        }
+    },
+
+    resetPassword: async (token,password) => {
+        set({ isLoading: true, error: null });
+        try {
+            const response = await axios.post(`${API_URL}/reset-password/${token}`,{password})
+            set({message:response.data.message, isLoading:false})
+        } catch (error) {
+            set({
+                error:
+                error.response?.data?.message ||
+                "Error sending forget-password email",
+                isLoading: false,
+            });
+            throw error;
+        }
+    }
 }));
